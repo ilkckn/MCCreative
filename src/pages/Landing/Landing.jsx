4c13a7174aeffdd6b656855a15e0a8bb3f3edfdd
Musa Cekcen <cekcen14@gmail.com>
2026-06-04 16:53:00 +0200

    footer updated

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -73,7 +73,7 @@ function Landing() {
           <div className="metrics-grid">
             <div className="metric-card">
               <div className="metric-value">
-                2<span>+</span>
+                3<span>+</span>
               </div>
               <p className="metric-label">{t("landing.metric_projects")}</p>
             </div>
